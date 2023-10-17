bbdb99d824822abc7e8ffd660d4ecfd6ffb48645
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-10-17 09:32:05 +0300

    Task0

--- Tyuiu.VolodinaAA.Sprint1.Task0.V16.Lib/DataService.cs
+++ Tyuiu.VolodinaAA.Sprint1.Task0.V16.Lib/DataService.cs
@@ -7,7 +7,7 @@ using tyuiu.cources.programming.interfaces.Sprint1;//
 
 namespace Tyuiu.VolodinaAA.Sprint1.Task0.V16.Lib
 {
-    public class DataService : ISprint1Task0V16
+    public class DataService : ISprint1Task0V16  
     {
         public double Calculate()
         {
